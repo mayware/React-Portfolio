1d969ba4ab8b4a51e63c459b66b81608389f8141
tofa93 <tofig.valiyev1993@gmail.com>
2023-06-26 00:37:48 +0200

    Added few changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,19 @@
 import '../styles/home.css'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div className="content">
             <div className="brief-card">
                 <div className="brief-card-info">
-                    <div className="brief-info-welcome-text">hello, I'm</div>
-                    <div className="typewriter">
-                        <div class="typed-out">Front-end React JS Developer</div>
+                    <div className="brief-card-text">
+                        <div className="brief-info-welcome-text">hello, I'm</div>
+                        <div className="typewriter">
+                            <div class="typed-out">Front-end React JS Developer</div>
+                        </div>
+                    </div>
+                    <div className="contact-link-area">
+                        <Link to="/contact" className='contact-link'>Contact me</Link>
                     </div>
                 </div>
             </div>
