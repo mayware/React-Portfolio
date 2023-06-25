3e1d29249fea2c3ad3ddacc1ea6dead288124f68
tofa93 <tofig.valiyev1993@gmail.com>
2023-06-25 21:53:40 +0200

    Added few more features

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,12 @@ const Home = () => {
     return (
         <div className="content">
             <div className="brief-card">
-
+                <div className="brief-card-info">
+                    <div className="brief-info-welcome-text">hello, I'm</div>
+                    <div className="typewriter">
+                        <div class="typed-out">Front-end React JS Developer</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
