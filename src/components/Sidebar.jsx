38a7806a0194bd9bb26f8240d9545c11968203a2
tofa93 <tofig.valiyev1993@gmail.com>
2023-06-26 20:18:41 +0200

    Added few more features

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,42 @@
 import '../styles/sidebar.css'
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
     return (
         <aside className="sidebar">
             <div className="sidebar-inner">
-
+                <NavLink to="/" className="sidebar-link">
+                    <div className="sidebar-link-inner">
+                        <span className='sidebar-link-icon'>
+                            <i class="fa-solid fa-house"></i>
+                        </span>
+                        <span className="sidebar-link-title">Home</span>
+                    </div>
+                </NavLink>
+                <NavLink to="/about" className="sidebar-link">
+                    <div className="sidebar-link-inner">
+                        <span className='sidebar-link-icon'>
+                            <i class="fa-solid fa-circle-question"></i>
+                        </span>
+                        <span className="sidebar-link-title">About</span>
+                    </div>
+                </NavLink>
+                <NavLink to="/portfolio" className="sidebar-link">
+                    <div className="sidebar-link-inner">
+                        <span className='sidebar-link-icon'>
+                            <i class="fa-solid fa-diagram-project"></i>
+                        </span>
+                        <span className="sidebar-link-title">Portfolio</span>
+                    </div>
+                </NavLink>
+                <NavLink to="/contact" className="sidebar-link">
+                    <div className="sidebar-link-inner">
+                        <span className='sidebar-link-icon'>
+                            <i class="fa-solid fa-envelope"></i>
+                        </span>
+                        <span className="sidebar-link-title">Contacts</span>
+                    </div>
+                </NavLink>
             </div>
         </aside>
     );
