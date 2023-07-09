1e3418ba5c9522ee2c04edbc04121ab4a3195756
tofa93 <tofig.valiyev1993@gmail.com>
2023-07-10 01:23:05 +0200

    Added few sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,10 +4,11 @@ const About = () => {
     return (
         <div className="content">
             <div className="about-box">
+                <div className="brief-info">
+                    <div className="brief-info-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. In laborum labore maiores ducimus quis quidem praesentium veritatis dolorum temporibus. Expedita magni rerum suscipit, et in, molestiae ducimus ea quibusdam deleniti cum, quo labore illum voluptates omnis qui neque eligendi officia cumque eos unde alias repudiandae nulla. Sunt, ducimus! Possimus, doloribus.</div>
+                    <div className="brief-info-image"></div>
+                </div>
                 <div className="skills-box">
-                    <div className="skills-header">
-                        <span className="skills-header-title">Tech stack</span>
-                    </div>
                     <div className="skills-icons">
                         <div className="skill-icon-box">
                             <i class="fa-brands fa-square-js js-icon"></i>
@@ -30,10 +31,25 @@ const About = () => {
                         <div className="skill-icon-box">
                             <i class="fa-brands fa-bootstrap bootstrap-icon"></i>
                         </div>
+                        <div className="skill-icon-box">
+                            <i class="fa-brands fa-square-git git-icon"></i>
+                        </div>
                     </div>
                 </div>
-                <div className="about-info"></div>
-
+                <div className="commercial-experience-box">
+                    <div className="comm-exp-position">
+                        <div className="comm-exp-image"></div>
+                        <div className="comm-exp-info"></div>
+                    </div>
+                    <div className="comm-exp-position">
+                        <div className="comm-exp-image"></div>
+                        <div className="comm-exp-info"></div>
+                    </div>
+                    <div className="comm-exp-position">
+                        <div className="comm-exp-image"></div>
+                        <div className="comm-exp-info"></div>
+                    </div>
+                </div>
             </div>
         </div>
     );
